3519156ba5fc2af91e481b860008d62353346eaa
Mark Brough <mark.brough@gmail.com>
2016-12-05 09:19:14 +0100

    Push rates to Github daily

--- get_parse_rates.sh
+++ get_parse_rates.sh
@@ -1,3 +1,6 @@
+cd /home/admin/exchange-rates/exchange-rates-usd
 wget https://fred.stlouisfed.org/categories/94/downloaddata/INTLFXD_csv_2.zip -O data/INTLFXD_csv_2.zip
 python run.py
+git commit data/consolidated.csv -m "Update consolidated CSV"
+git push
 
