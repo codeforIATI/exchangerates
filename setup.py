14b0d68766f9f4517ff5ec68b0f4f99bc3fc212b
Mark Brough <mark.brough@gmail.com>
2017-04-11 01:13:29 +0200

    Try fixing version number again

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='exchangerates',
-    version='0.1.0',
+    version='0.1.01',
     description="A module to make it easier to handle historical exchange rates",
     long_description="",
     classifiers=[
