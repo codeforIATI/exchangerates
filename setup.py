3cde59d9b7fa90c3036985c6e6516c6f55c883e0
Mark Brough <mark.brough@gmail.com>
2018-04-10 17:44:00 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='exchangerates',
-    version='0.1.22',
+    version='0.2.0',
     description="A module to make it easier to handle historical exchange rates",
     long_description="",
     classifiers=[
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        'Programming Language :: Python :: 2.7'
+        'Programming Language :: Python :: 3.6'
     ],
     author='Mark Brough',
     author_email='mark@brough.io',
