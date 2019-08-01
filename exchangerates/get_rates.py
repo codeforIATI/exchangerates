8e6778b00936eba71a2ae5ba7a953d18b9c60437
Mark Brough <mark.brough@gmail.com>
2019-08-01 19:19:30 +0200

    Change retrieval from FRED to use JSON API and add some debugging lines

--- exchangerates/get_rates.py
+++ exchangerates/get_rates.py
@@ -1,75 +1,61 @@
 import csv
-import zipfile
 import requests
 import datetime
 import re
+import json
 
 from lxml import etree
-from six import next, BytesIO
 
 from .util import fred_countries_currencies, oecd_countries_currencies
 
 
-FRED_RATES = "https://fred.stlouisfed.org/categories/94/downloaddata/INTLFXD_csv_2.zip"
 OECD_RATES = "http://stats.oecd.org/restsdmx/sdmx.ashx/GetData/MEI_FIN/CCUS.AUS+AUT+BEL+CAN+CHL+CZE+DNK+EST+FIN+FRA+DEU+GRC+HUN+ISL+IRL+ISR+ITA+JPN+KOR+LVA+LUX+MEX+NLD+NZL+NOR+POL+PRT+SVK+SVN+ESP+SWE+CHE+TUR+GBR+USA+EA19+SDR+NMEC+BRA+CHN+COL+CRI+IND+IDN+RUS+ZAF.M/all?startTime=1950-01"
 
-
-class ZipExtFileWrapper:
-    def __init__(self, zef):
-        self.zef = zef
-
-    def __iter__(self):
-        while True:
-            try:
-                yield next(self.zef).decode('utf-8')
-            except StopIteration:
-                break
-
-    def close(self):
-        return self.zef.close()
+FRED_API_KEY = "0ac22634c3acb260a59293c864df5e34"
+FRED_CATEGORY_API = "https://api.stlouisfed.org/fred/category/series?category_id={}&api_key={}&file_type=json"
+FRED_SERIES_OBSERVATIONS_API = "https://api.stlouisfed.org/fred/series/observations?series_id={}&api_key={}&file_type=json"
+FRED_DAILY_CATEGORY = 94
 
 
 def get_fred_rates(outfp, writer):
-    def extract_file(zfo, id_, from_currency, to_currency, freq):
-        fp = 'INTLFXD_csv_2/data/{}'.format(id_)
-        fo = ZipExtFileWrapper(zfo.open(fp))
-        reader = csv.reader(fo)
-        next(reader)
+    def get_rates(id_, from_currency, to_currency, freq):
+        print("Retrieving rates from FRED for {}".format(id_))
+        r_series = requests.get(FRED_SERIES_OBSERVATIONS_API.format(id_, FRED_API_KEY))
+        r_series_json = r_series.json()
+        if r_series_json['count'] > r_series_json['limit']:
+            raise
         country = {True: to_currency,
                    False: from_currency}[from_currency == "U.S."]
-        for row in reader:
-            if row[1] == ".":
+        print("Getting data for currency {}".format(fred_countries_currencies[country]))
+        for row in r_series_json['observations']:
+            if row['value'] == ".":
                 continue
             if from_currency == "U.S.":
-                row[1] = round(1/float(row[1]), 4)
-            outrow = row + [fred_countries_currencies[country], freq, "FRED"]
+                row['value'] = 1/float(row['value'])
+            outrow = [row['date'], row['value'], fred_countries_currencies[country], freq, "FRED"]
             writer.writerow(outrow)
-        fo.close()
 
-    def read_files(zfo):
-        fo = zfo.open('INTLFXD_csv_2/README_SERIES_ID_SORT.txt')
-        for line in fo.readlines():
-            line = line.decode('utf-8')
-            if not line.startswith("DEX"):
+    def retrieve_rates():
+        r = requests.get(FRED_CATEGORY_API.format(FRED_DAILY_CATEGORY, FRED_API_KEY))
+        categories_json = r.json()
+        for series in categories_json["seriess"]:
+            # We only want real daily exchange rates
+            if not series['id'].startswith("DEX"):
                 continue
-            columns = line.split(';')
-            id_ = columns[0].strip()
             from_currency, to_currency = re.match(
-                            "(.*) / (.*) Foreign Exchange Rate", columns[1]
-                            ).groups()
-            freq = columns[3].strip()
+                    "(.*) / (.*) Foreign Exchange Rate", series['title']
+                    ).groups()
+            freq = series['frequency_short']
             try:
-                extract_file(zfo, id_, from_currency, to_currency, freq)
+                get_rates(series['id'], from_currency, to_currency, freq)
             except Exception as inst:
-                print(id_)
+                print(series['id'])
                 raise
 
-    r = requests.get(FRED_RATES)
-    zfo = zipfile.ZipFile(BytesIO(r.content))
-    read_files(zfo)
+    retrieve_rates()
 
 
-def get_oecd_rates(outfp, writer):
+def get_oecd_rates(outfp, writer, include_all_dates):
     def make_date(value):
         return datetime.datetime.strptime(value, "%Y-%m-%d")
 
@@ -80,17 +66,15 @@ def get_oecd_rates(outfp, writer):
         indata = list(map(lambda row: row, reader))
         outfp_file.close()
 
-        currencies = dict(map(lambda currency:
-                              (currency, None),
-                  list(set(map(lambda row: row["Currency"], indata)))))
+        currencies = dict(map(lambda row: (row["Currency"], None), indata))
 
         for currency in currencies:
+            filtered_currencies = filter(lambda x: x['Currency'] == currency, indata)
             currency_dates = list(map(lambda y:
                       make_date(y["Date"]),
-                    filter(lambda x: x['Currency'] == currency, indata)
+                    filtered_currencies
             ))
             currencies[currency] = min(currency_dates)
-
         return currencies
 
     def get_OECD_data(writer, currencies_dates):
@@ -110,22 +94,25 @@ def get_oecd_rates(outfp, writer):
             for obs in serie.findall("ns:Obs", namespaces=nsmap):
                 date = "{}-01".format(obs.find("ns:Time", namespaces=nsmap).text)
                 value = obs.find("ns:ObsValue", namespaces=nsmap).get("value")
-                if make_date(date) < min_currency_date:
+                if include_all_dates or (make_date(date) < min_currency_date):
                     writer.writerow([date, value, oecd_countries_currencies.get(currency), "M", "OECD"])
 
+    print("Getting rates from OECD")
     currencies_dates = get_earliest_dates()
     get_OECD_data(writer, currencies_dates)
 
 
-def update_rates(out_filename):
+def update_rates(out_filename, include_all_dates=True):
     outfp = out_filename
     outfp_f = open(outfp, 'w')
     writer = csv.writer(outfp_f)
     writer.writerow(['Date', 'Rate', 'Currency', 'Frequency', 'Source'])
     get_fred_rates(outfp, writer)
+    outfp_f.close()
 
+    outfp_f = outfp_f = open(outfp, 'a')
     writer = csv.writer(outfp_f)
-    get_oecd_rates(outfp, writer)
+    get_oecd_rates(outfp, writer, include_all_dates)
     outfp_f.close()
 
 
