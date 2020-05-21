9cea258dc69ed56849cf78c8483b4b5497523779
Mark Brough <mark.brough@gmail.com>
2020-05-21 11:12:47 +0200

    Allow later versions of libraries, e.g. lxml

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='exchangerates',
-    version='0.3.2',
+    version='0.3.3',
     description="A module to make it easier to handle historical exchange rates",
     long_description="",
     classifiers=[
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.7.5'
     ],
     author='Mark Brough',
     author_email='mark@brough.io',
@@ -21,9 +21,9 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'lxml == 4.4.0',
-        'requests == 2.22.0',
-        'six == 1.12.0'
+        'lxml >= 4.4.0',
+        'requests >= 2.22.0',
+        'six >= 1.12.0'
     ],
     entry_points={
     }
