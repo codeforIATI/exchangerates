7db2c384f1cef4d00ebc1fabb50a105396afc38e
Mark Brough <mark.brough@gmail.com>
2024-09-15 00:36:40 +0000

    Update Python versions supported and update version numbers

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='exchangerates',
-    version='0.3.4',
+    version='0.3.5',
     description="A module to make it easier to handle historical exchange rates",
     long_description="",
     classifiers=[
@@ -11,7 +11,11 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         'Programming Language :: Python :: 3.6',
-	'Programming Language :: Python :: 3.7'
+	    'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
     author='Mark Brough',
     author_email='mark@brough.io',
